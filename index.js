d8032a0804182966da689edbfc8cbad4fe54b847
vileda <vileda@piratenfraktion-nrw.de>
2013-08-22 14:31:45 +0200

    bugs

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ exports.eejsBlock_indexWrapper = function (hook_name, args, cb) {
     padstring += "<table>";
     pads.forEach(function(item){
         var pad = new Pad(item);
+        var padTimestamp;
         pad.init(null, function(err) { if(!err) { console.log(pad.getLastEdit(function(err, timestamp) {
           console.log("small_list: " + timestamp);
         })); } });
         padstring += '<tr>';
         padstring += '<td><a href="/p/' + item + '">' + item + '</a></td>';
-        padstring += '<td>' + pad.getLastEdit(function(err, meta, stamp){ console.log("small_list: " + meta + ", " + stamp); }) + '</td>';
+        padstring += '<td>' + pad.meta.timestamp + '</td>';
         padstring += '</tr>';
         console.log("small_list: " + JSON.stringify(pad));
     });
